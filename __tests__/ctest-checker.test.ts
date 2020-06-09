c1d0afd509007a8b54302c95db5a75f060c38e27
Akira KAWAGUCHI <kawaguchi@nautilus-technologies.com>
2020-06-09 09:54:33 +0900

    Add testcase

--- __tests__/ctest-checker.test.ts
+++ __tests__/ctest-checker.test.ts
@@ -90,3 +90,19 @@ test('no suite in suites', async () => {
   console.log(checker.result);
   console.log(checker.summary);
 })
+
+test('multi folder', async () => {
+  process.env['GITHUB_WORKSPACE'] = process.cwd();
+  process.env['INPUT_CTEST-INPUT'] = "__tests__/ctest_4warnings/build*/*gtest_result.xml";
+
+  const checker:CTestChecker = new CTestChecker()
+  const doIf = await checker.doIf()
+  const annotations = await checker.parse()
+
+  expect(doIf).toBe(true)
+  expect(annotations.length).toBe(4)
+
+  console.log(annotations);
+  console.log(checker.result);
+  console.log(checker.summary);
+})
